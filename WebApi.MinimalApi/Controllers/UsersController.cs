8c3bb71608d5921350d84178b7de65e374a98de6
OK <143250301+OK219@users.noreply.github.com>
2025-09-30 15:59:46 +0500

    task 1

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 
@@ -8,15 +9,24 @@ namespace WebApi.MinimalApi.Controllers;
 [ApiController]
 public class UsersController : Controller
 {
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
+        _userRepository = userRepository;
+        _mapper = mapper;
     }
 
     [HttpGet("{userId}")]
-    public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
+    [Produces("application/json", "application/xml")]
+    public ActionResult<UserDto>? GetUserById([FromRoute] Guid userId)
     {
-        throw new NotImplementedException();
+        var userEntity = _userRepository.FindById(userId);
+        if (userEntity is null) return NotFound();
+        var userDto = _mapper.Map<UserDto>(userEntity);
+        return Ok(userDto);
     }
 
     [HttpPost]
--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,10 +1,34 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
 builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options => {
+    .ConfigureApiBehaviorOptions(options =>
+    {
         options.SuppressModelStateInvalidFilter = true;
         options.SuppressMapClientErrors = true;
     });
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+
+builder.Services.AddControllers(options =>
+{
+    // Этот OutputFormatter позволяет возвращать данные в XML, если требуется.
+    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+    // Эта настройка позволяет отвечать кодом 406 Not Acceptable на запросы неизвестных форматов.
+    options.ReturnHttpNotAcceptable = true;
+    // Эта настройка приводит к игнорированию заголовка Accept, когда он содержит */*
+    // Здесь она нужна, чтобы в этом случае ответ возвращался в формате JSON
+    options.RespectBrowserAcceptHeader = true;
+});
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>()
+        .ForMember(x => x.FullName,
+            y => y.MapFrom(user => $"{user.LastName} {user.FirstName}"));
+}, new System.Reflection.Assembly[0]);
 
 var app = builder.Build();
 
